f12454fe2b6fe2e90433a21d0aef1320fcaee1cd
bkdragon0228 <rlaqjarb2067@naver.com>
2022-08-02 14:21:21 +0900

    추가 : 기본타입#2

--- learn-typescript-master/class-note/1_type-basic.ts
+++ learn-typescript-master/class-note/1_type-basic.ts
@@ -7,5 +7,29 @@ let arr: Array<number> = [1,2,3]; // 넘버타입이 들어가는 배열
 let heroes: Array<string> = ['capt', 'thor','hulk']; // 안에 숫자를 넣으면 오류 발생
 
 // 배열 선언 다른 방식
-let items: number[] = [1,2,3]
+let items: number[] = [1,2,3];
+
+// ts 튜플
+// 배열 인덱스에 타입까지 설정하는 것
+let address: [string, number] = ['gangnam', 100];
+
+// ts 객체
+let obj: object = {};
+let person: object = {
+    name : 'capt',
+    age : 'hello'
+    // 따로 오류가 나지 않음
+};
+// 구체적으로 가능
+let person2: {name : string, age : number} = {
+    name : 'bk',
+    age: 26,
+}
+
+// ts 진위값
+let show: boolean = true;
+
+
+
+
 
