a5cc1e53fda64d605e908d76ccea89ff437bdaa2
bkdragon0228 <rlaqjarb2067@naver.com>
2022-08-02 14:14:33 +0900

    추가  : ts식 기본타입 선언 방식

---
+++ learn-typescript-master/class-note/1_type-basic.ts
@@ -0,0 +1,11 @@
+// 기본 타입
+
+// ts식 선언 방식
+const str2: string = 'hello';
+let num: number = 10;
+let arr: Array<number> = [1,2,3]; // 넘버타입이 들어가는 배열
+let heroes: Array<string> = ['capt', 'thor','hulk']; // 안에 숫자를 넣으면 오류 발생
+
+// 배열 선언 다른 방식
+let items: number[] = [1,2,3]
+
