fb3b321a598b871725a003edf761574d2b94c4e1
bkdragon0228 <rlaqjarb2067@naver.com>
2022-08-03 11:59:55 +0900

    추가 : 드롭 다운 예제 제네릭 사용전
    비슷한 인터페이스도 두번 선언해야하고 또 새로운 타입이 생기면 대응하는
    코드도 필요하다.

--- learn-typescript-master/example/dropdown-generic.ts
+++ learn-typescript-master/example/dropdown-generic.ts
@@ -1,26 +1,41 @@
-const emails = [
-  { value: 'naver.com', selected: true },
-  { value: 'gmail.com', selected: false },
-  { value: 'hanmail.net', selected: false },
+interface Email {
+    value: string;
+    selected: boolean;
+}
+
+interface ProductNumber {
+    value: number;
+    selected: boolean;
+}
+const emails: Email[] = [
+    { value: 'naver.com', selected: true },
+    { value: 'gmail.com', selected: false },
+    { value: 'hanmail.net', selected: false },
 ];
 
-const numberOfProducts = [
-  { value: 1, selected: true },
-  { value: 2, selected: false },
-  { value: 3, selected: false },
+const numberOfProducts: ProductNumber[] = [
+    { value: 1, selected: true },
+    { value: 2, selected: false },
+    { value: 3, selected: false },
 ];
 
-function createDropdownItem(item) {
-  const option = document.createElement('option');
-  option.value = item.value.toString();
-  option.innerText = item.value.toString();
-  option.selected = item.selected;
-  return option;
+// item의 타입이 다를 수 있음 두개다 수용할 수 있게해야 한다.
+function createDropdownItem(item: Email | ProductNumber) {
+    const option = document.createElement('option');
+    option.value = item.value.toString();
+    option.innerText = item.value.toString();
+    option.selected = item.selected;
+    return option;
 }
 
 // NOTE: 이메일 드롭 다운 아이템 추가
 emails.forEach(function (email) {
-  const item = createDropdownItem(email);
-  const selectTag = document.querySelector('#email-dropdown');
-  selectTag.appendChild(item);
+    const item = createDropdownItem(email);
+    const selectTag = document.querySelector('#email-dropdown');
+    selectTag.appendChild(item);
+});
+
+// 상품 개수 트롭다운 아이템 추가
+numberOfProducts.forEach((product) => {
+    const item = createDropdownItem(product);
 });
