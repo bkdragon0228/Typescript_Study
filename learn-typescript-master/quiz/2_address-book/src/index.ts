f948f8934088d0c577e5d6f240705468d3ac9128
bkdragon0228 <rlaqjarb2067@naver.com>
2022-08-03 14:11:37 +0900

    추가 : 2_address 프로젝트 타이핑

--- learn-typescript-master/quiz/2_address-book/src/index.ts
+++ learn-typescript-master/quiz/2_address-book/src/index.ts
@@ -1,20 +1,24 @@
+// 전화번호
 interface PhoneNumberDictionary {
   [phone: string]: {
     num: number;
   };
 }
 
+// 전화 번호부
 interface Contact {
   name: string;
   address: string;
   phones: PhoneNumberDictionary;
 }
 
-// api
+// api를 모방
 // TODO: 아래 함수의 반환 타입을 지정해보세요.
-function fetchContacts() {
+// Promise<unknown>을 해결해야한다.
+// resolve된 값이 반환 타입으로 들어가야한다.
+function fetchContacts(): Promise<Contact[]> {
   // TODO: 아래 변수의 타입을 지정해보세요.
-  const contacts = [
+  const contacts: Contact[] = [
     {
       name: 'Tony',
       address: 'Malibu',
@@ -57,42 +61,43 @@ function fetchContacts() {
 // main
 class AddressBook {
   // TODO: 아래 변수의 타입을 지정해보세요.
-  contacts = [];
+  contacts: Contact[] = [];
 
+  // constructor엔 타입 정의 필요없음
   constructor() {
     this.fetchData();
   }
 
-  fetchData() {
+  fetchData(): void {
     fetchContacts().then(response => {
-      this.contacts = response;
+      this.contacts = response; // fetchContact를 받아옴
     });
   }
 
   /* TODO: 아래 함수들의 파라미터 타입과 반환 타입을 지정해보세요 */
-  findContactByName(name) {
+  findContactByName(name: string): Contact[] {
     return this.contacts.filter(contact => contact.name === name);
   }
 
-  findContactByAddress(address) {
+  findContactByAddress(address: string): Contact[] {
     return this.contacts.filter(contact => contact.address === address);
   }
 
-  findContactByPhone(phoneNumber, phoneType: string) {
+  findContactByPhone(phoneNumber: number, phoneType: string): Contact[] {
     return this.contacts.filter(
       contact => contact.phones[phoneType].num === phoneNumber
     );
   }
 
-  addContact(contact) {
+  addContact(contact: Contact): void {
     this.contacts.push(contact);
   }
 
-  displayListByName() {
+  displayListByName(): string[] {
     return this.contacts.map(contact => contact.name);
   }
 
-  displayListByAddress() {
+  displayListByAddress(): string[] {
     return this.contacts.map(contact => contact.address);
   }
   /* ------------------------------------------------ */
