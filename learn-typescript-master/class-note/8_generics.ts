9f9564389032279a0a0edfb51baf3f637550b447
bkdragon0228 <rlaqjarb2067@naver.com>
2022-08-03 11:45:36 +0900

    추가 : 제네릭 기본, 장점

---
+++ learn-typescript-master/class-note/8_generics.ts
@@ -0,0 +1,31 @@
+// function logText(text) {
+//     console.log(text);
+
+//     return text;
+// }
+
+// logText(10);
+// logText('하이');
+
+function logText<T>(text: T): T {
+    console.log(text);
+    return text;
+}
+
+// 호출할 때, 인자와 타입을 같이 넘긴다.
+logText<string>('하이');
+
+// 타입이 다른데 로직이 같은 함수 여러개를 만들수 있다.
+// 유니온 타입도 가능하지만,
+
+function sayText(text: string | number) {
+    console.log(text);
+    return text;
+}
+
+sayText('a');
+const num = sayText(10);
+// 타입 가드가 이뤄지지 않으면 반환값이 여전히 유니온타입이다.
+
+logText<string>('a');
+logText<number>(10);
