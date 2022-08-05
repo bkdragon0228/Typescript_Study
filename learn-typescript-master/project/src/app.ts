30288e2a32bdede8524331607ca84510fd3eba9f
bkdragon0228 <rlaqjarb2067@naver.com>
2022-08-05 11:19:30 +0900

    추가 : project 타이핑 완료

--- learn-typescript-master/project/src/app.ts
+++ learn-typescript-master/project/src/app.ts
@@ -49,7 +49,7 @@ function createSpinnerElement(id: string) {
 
 // state
 let isDeathLoading = false;
-const isRecoveredLoading = false;
+// const isRecoveredLoading = false;
 
 // api
 function fetchCovidSummary(): Promise<AxiosResponse<CovidSummaryResponse>> {
@@ -192,8 +192,9 @@ async function setupData() {
     setLastUpdatedTimestamp(data);
 }
 
-function renderChart(data: CountrySummaryResponse, labels: CountrySummaryInfo) {
-    const ctx = $('#lineChart').getContext('2d');
+function renderChart(data: number[], labels: string[]) {
+    const lineChart = $('#lineChart') as HTMLCanvasElement;
+    const ctx = lineChart.getContext('2d');
     Chart.defaults.color = '#f5eaea';
     Chart.defaults.font.family = 'Exo 2';
     new Chart(ctx, {
